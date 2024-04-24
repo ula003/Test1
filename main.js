6b3795ad2a3f4552f591a76f9fbc0c6fbf27e059
ula003 <nulugbek128@gmail.com>
2024-04-24 18:37:52 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// // console.log(1+2); //3
+// // console.log(1-2); //-1
+// // console.log(1*2); //2
+// // console.log(1/2); //0.5
+
+// // //**, %
+// // console.log(2 **4) // 16
+// // console.log(4 % 3);//1
+
+// // let a = '45';
+// // let b = 45;
+
+// // console.log(b + a); //4545
+
+// // console.log('45' == 45);
+// // console.log('42' == 45);
+// // console.log(2 == 2);
+
+// //math
+// const a = Math.round(45.9);
+// const b = Math.floor(45.9);
+// const c = Math.ceil(45.1);
+// const d = Math.pow( 2 , 6);
+// const e = Math.random( ) * 100;
+// const f = Math.abs(56, 10);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+
+// const age = prompt('Введите ваш возраст');
+// const isMarried = confirm('Вам есть 18?')
+
+// // console.log(age);
+
+// console.log(isMarried)
+// const abc = alert('Продолжать?')
+const a = prompt('Какой ваш баланс?')
+
+const b = prompt('Какой ваш доход?')
+
+const c = prompt('Какой ваш расход?')
+
+const d = ((b/a)*100)
+const e = ((c/a)*100)
+const f = alert('Ваши доходы ' + d + '% от вашего баланса.\nВаш расход составляет ' + e + '% вашего баланса')
